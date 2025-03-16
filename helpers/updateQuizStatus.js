fb825dca349447ac82bb0ca9b0a6bc2de5b50c8e
pavanleton <pavankumarkpkp@gmail.com>
2025-03-16 15:21:08 +0530

    feat: replace notion with airtable to fetch quiz

--- helpers/updateQuizStatus.js
+++ helpers/updateQuizStatus.js
@@ -1,26 +1,31 @@
-const { Status } = require("whatsapp-web.js")
-const { NOTION_QUIZ_DATABASE_ID, NOTION_SECRET } = require("../config/IDs")
+const { Status } = require("whatsapp-web.js");
+const { NOTION_QUIZ_DATABASE_ID, NOTION_SECRET } = require("../config/IDs");
 
-const n = require("@notionhq/client")
+const n = require("@notionhq/client");
+const Airtable = require("airtable");
 
 const notion = new n.Client({
-    auth: NOTION_SECRET
-})
+  auth: NOTION_SECRET,
+});
 
-const moveQuizStatusToDone = async (pageId) => {
-    return notion.pages.update({
-        page_id: pageId,
-        properties: {
-            Status: {
-                select: {
-                    name: "Done ✨"
-                }
-            }
-        }
-    })
+const moveQuizStatusToDone = async (recordId) => {
+  Airtable.configure({
+    apiKey:
+      "patkmOoiTsoEncDON.2602e1a475c01407ecdee696a5df6ae4512008e88da5da4997b4101aec6cab56",
+    endpointUrl: "https://api.airtable.com",
+  });
+  const base = Airtable.base("appAMjOmbK8EmBM6i");
 
-}
+  return base("Collection A").update([
+    {
+      id: recordId,
+      fields: {
+        Status: "Done",
+      },
+    },
+  ]);
+};
 
 module.exports = {
-    moveQuizStatusToDone
-}
+  moveQuizStatusToDone,
+};
