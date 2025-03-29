ac3e55f26828509936873a33068607ceb836b66f
pavanleton <pavankumarkpkp@gmail.com>
2025-03-29 23:46:38 +0530

    remove pg client definition

--- config/pg.ts
+++ config/pg.ts
@@ -1,10 +1,5 @@
-import { Client, Pool } from "pg";
+import { Pool } from "pg";
 import { DATABASE_URL } from "../config/constants";
-
-export const pgClient = new Client({
-  connectionString:
-    "postgresql://neondb_owner:npg_z2G3otfUHqkX@ep-old-star-a11ru1n8-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
-});
 
 export const pgPool = new Pool({
   connectionString: DATABASE_URL,
